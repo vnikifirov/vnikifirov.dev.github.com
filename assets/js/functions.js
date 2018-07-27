eefad4ca9edd3550aecf0d3d22b3bb8c8b3c398b
Vadim N <nikifovadim@gmail.com>
2018-07-27 17:24:30 +0300

    first commit

---
+++ assets/js/functions.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+  smoothScroll(1500); // 300
+});
+
+// smoothScroll function is applied from the document ready function
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+	    var target = $( $(this).attr('href') );
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top - 100
+	        }, duration);
+	    }
+	});
+}
